5253fc98d01afad5a0382b7c88d7f40b98f3872c
nhaan1110 <nhaan1110@gmail.com>
2026-08-10 16:31:56 +0700

    [AnNBN] add fiori frontend app (UI5) to CAP demo

---
+++ app/zbooks/webapp/controller/Main.controller.js
@@ -0,0 +1,11 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+], function (Controller) {
+    "use strict";
+
+    return Controller.extend("zbooks.controller.Main", {
+        onInit: function () {
+            // Hàm khởi tạo tự động chạy khi View được load
+        }
+    });
+});
